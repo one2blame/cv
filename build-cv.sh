8b0aeeef6a640b25b8e91d9e10059621f6748ac9
one2blame <contactme@one2bla.me>
2022-08-07 23:58:04 -0400

    add artifacts

--- build-cv.sh
+++ build-cv.sh
@@ -17,12 +17,22 @@ format_cv () {
 }
 
 
-build_cv () {
+build_pdf () {
     $docker run --volume `pwd`:/data one2blame-cv pdflatex cv.tex
+}
+
+
+build_html () {
+    $docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex-alpine pdf2htmlEX --zoom 1.3 cv.pdf
 }
 
 
 check_deps
 build_container
 format_cv
-build_cv
+build_pdf
+build_html
+
+mkdir -p docs
+mv cv.pdf docs
+mv cv.html docs
